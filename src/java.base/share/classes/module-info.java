5af1ffcf7dc77c645ccbe6204fd9c69a771938c0
akashche <akashche@redhat.com>
2021-09-20 14:14:54 -0700

    revert rh1996182-login_to_nss_software_token
    This reverts commit 4f70552fda002f85dc0c1d74dd9f009faa605060.

--- src/java.base/share/classes/module-info.java
+++ src/java.base/share/classes/module-info.java
@@ -151,7 +151,6 @@ module java.base {
         java.management,
         java.naming,
         java.rmi,
-        jdk.crypto.cryptoki,
         jdk.crypto.ec,
         jdk.jartool,
         jdk.jlink,
